cd616972f43b9a81089a2b238a93255867bc1208
Matthew Huang <admin@Admins-MacBook-Pro.local>
2021-09-22 21:59:49 -0700

    replace words & color with red completed

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
+
+const couchify = function (string){
+
+    //search for is and get the index after
+
+    const nouns = ['my', 'a', 'the', 'their', 'your', 'our', 'those'];
+
+    const splitString = string.split(' ');
+
+    for (let i = 0; i < splitString.length; i += 1){
+        if (nouns.includes(splitString[i])){
+        splitString[i + 1] = '<a style="color: red">couch</a>';
+        }
+    }
+
+    let newString = splitString.join(' ');
+
+    return newString;
+}
+
+
 const header = document.createElement('h1');
 header.innerHTML = 'TESTING';
 document.body.appendChild(header);
@@ -10,28 +31,13 @@ for (let i = 0; i < imgArr.length - 1; i += 1){
   imgArr[i].src = 'https://github.com/matthewhuang24/chromeExtension/blob/master/images/couch.png?raw=trueimages/couch.png';
 }
 
+const elArr= ['h1', 'p'];
 
-const divNouns = document.querySelectorAll("div")
+for(const el of elArr){
+    const elNouns = document.querySelectorAll(el);
 
-let str='my phone is the best';
-
-const couchify = function (string){
-
-    //search for is and get the index after
-
-    const nouns = ['my', 'a', 'the', 'their', 'your', 'our', 'those'];
-
-    const splitString = string.split(' ');
-
-    for (let i = 0; i < splitString.length; i += 1){
-        if (nouns.contains(splitString[i])){
-        splitString[i + 1] = 'couch';
-        }
+    for (let i = 0; i < elNouns.length - 1; i += 1){
+        console.log(elNouns[i]);
+        elNouns[i].innerHTML = couchify(elNouns[i].innerText);
     }
-
-    let newString = splitString.join(' ');
-
-    return newString;
 }
-
-console.log(couchify(str));

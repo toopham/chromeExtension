99e82568f454c3a506f2bf2ea09eb890351a59b4
Matthew Huang <admin@Admins-MacBook-Pro.local>
2021-09-22 20:39:39 -0700

    rework

---
+++ main.js
@@ -0,0 +1,10 @@
+const header = document.createElement('h1');
+header.innerHTML = 'TESTING QUAN COUCH';
+document.body.appendChild(header);
+
+const imgArr = document.querySelectorAll("img");
+
+
+for (let i = 0; i < imgArr.length; i += 1){
+  img[i].src = "/images/couch.png";
+}

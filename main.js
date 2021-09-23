b1c39276a1ec4bebbfc752739f1b000a2b25d10f
Matthew Huang <admin@Admins-MacBook-Pro.local>
2021-09-22 21:35:41 -0700

    image replacer complete

--- main.js
+++ main.js
@@ -6,9 +6,32 @@ document.body.appendChild(header);
 
 const imgArr = document.querySelectorAll("img");
 
-console.log(imgArr);
-
 for (let i = 0; i < imgArr.length - 1; i += 1){
   imgArr[i].src = 'https://github.com/matthewhuang24/chromeExtension/blob/master/images/couch.png?raw=trueimages/couch.png';
-  console.log(imgArr[i].src);
 }
+
+
+const divNouns = document.querySelectorAll("div")
+
+let str='my phone is the best';
+
+const couchify = function (string){
+
+    //search for is and get the index after
+
+    const nouns = ['my', 'a', 'the', 'their', 'your', 'our', 'those'];
+
+    const splitString = string.split(' ');
+
+    for (let i = 0; i < splitString.length; i += 1){
+        if (nouns.contains(splitString[i])){
+        splitString[i + 1] = 'couch';
+        }
+    }
+
+    let newString = splitString.join(' ');
+
+    return newString;
+}
+
+console.log(couchify(str));

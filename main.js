858cdab8c559ccb0a7d34c54de02b806d316e714
Matthew Huang <admin@Admins-MacBook-Pro.local>
2021-09-22 21:02:09 -0700

    couch

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 const header = document.createElement('h1');
-header.innerHTML = 'TESTING QUAN COUCH';
+header.innerHTML = 'TESTING';
 document.body.appendChild(header);
+
+
 
 const imgArr = document.querySelectorAll("img");
 
+console.log(imgArr);
 
-for (let i = 0; i < imgArr.length; i += 1){
-  img[i].src = "/images/couch.png";
+for (let i = 0; i < imgArr.length - 1; i += 1){
+  imgArr[i].src = 'https://github.com/matthewhuang24/chromeExtension/blob/master/images/couch.png?raw=trueimages/couch.png';
+  console.log(imgArr[i].src);
 }
